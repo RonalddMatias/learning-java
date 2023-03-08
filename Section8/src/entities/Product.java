3367b20c205181b6d3c9e4374e45246bafc9c2b4
RonalddMatias <ronaldd.feliph.matias.costa@ccc.ufcg.edu.br>
2023-03-07 22:08:45 -0300

    implementação de exercicio no padrão UML

---
+++ Section8/src/entities/Product.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public Product() {
+		
+	}
+	
+	// Métodos
+	public double TotalValueInStock() {
+		return quantity * price;
+	}
+	
+	public void AddProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void RemoveProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String resumoDeCompra() {
+		return String.format("%s, $ %.2f, %d units, TOTAL: $ %.2f\n",name, price, quantity, TotalValueInStock());
+	}
+}
---
+++ Section8/src/exercicio/ProductTeste.java
@@ -0,0 +1,41 @@
+package exercicio;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class ProductTeste {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Product p = new Product();
+		
+		System.out.println("Enter a product data: ");
+		System.out.print("Name: ");
+		p.name = sc.nextLine();
+		
+		System.out.print("Price: ");
+		p.price = sc.nextDouble();
+		
+		System.out.print("Quantity in stock: ");
+		p.quantity = sc.nextInt();
+		
+		System.out.println("Product data: " + p.resumoDeCompra());
+		
+		System.out.print("Enter the number of products to be added in stock: ");
+		int add = sc.nextInt();
+		p.AddProducts(add);
+		System.out.println("Update data: " + p.resumoDeCompra());
+		
+		System.out.print("Enter the number of products to be remove in stock: ");
+		int rm = sc.nextInt();
+		p.RemoveProducts(rm);
+		System.out.println("Update data: " + p.resumoDeCompra());
+		sc.close();
+
+	}
+
+}
